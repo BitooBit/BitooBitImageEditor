9a919f776e042bc1482b7a0d962c39dd4508a529
dimonovdd <dimovde@yandex.ru>
2019-12-20 00:21:35 +0300

    upd

---
+++ src/BitooBitImageEditor.UWP/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("BitooBitImageEditor.UWP")]
+[assembly: AssemblyDescription("simple image editor for xamarin")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("BitooBit")]
+[assembly: AssemblyProduct("BitooBitImageEditor.UWP")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("BitooBit")]
+[assembly: AssemblyCulture("")]
+
+// Сведения о версии для сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
---
+++ src/SampleImageEditor/SampleImageEditor/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using BitooBitImageEditor;
+using System;
+using System.IO;
+using Xamarin.Forms;
+
+namespace SampleImageEditor
+{
+
+
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.BindingContext = this;
+        }
+
+
+        private byte[] data;
+
+        private async void GetEditedImage_Clicked(object sender, EventArgs e)
+        {
+            byte[] data = await ImageEditor.Instance.GetEditedImage();
+            this.data = data;
+            if (data != null)
+            {
+                MyImage.Source = ImageSource.FromStream(() => new MemoryStream(data));
+            }
+        }
+
+
+        private async void SaveImage_Clicked(object sender, EventArgs e)
+        {
+            string message;
+            if (data != null)
+            {
+                if (await ImageEditor.Instance.ImageHelper.SaveImageAsync(data, $"img{DateTime.Now.ToString()}.png"))
+                    message = "Successfully!!!";
+                else
+                    message = "Unsuccessfully!!!";
+            }
+            else
+                message = "You should edit the image";
+              await DisplayAlert("", message, "Ok");
+        }
+
+    }
+}
