4a0da91b9c71347f4d8cff5884c437a9cdd4c51f
dimonovdd <dimovde@yandex.ru>
2019-12-20 11:39:22 +0300

    UPD

--- src/SampleImageEditor/SampleImageEditor.Android/Resources/Resource.designer.cs
+++ src/SampleImageEditor/SampleImageEditor.Android/Resources/Resource.designer.cs
@@ -9332,11 +9332,14 @@ namespace SampleImageEditor.Droid
 			// aapt resource value: 0x7F0C0000
 			public const int icon = 2131492864;
 			
+			// aapt resource value: 0x7F0C0002
+			public const int iconapp = 2131492866;
+			
 			// aapt resource value: 0x7F0C0001
 			public const int icon_round = 2131492865;
 			
-			// aapt resource value: 0x7F0C0002
-			public const int launcher_foreground = 2131492866;
+			// aapt resource value: 0x7F0C0003
+			public const int launcher_foreground = 2131492867;
 			
 			static Mipmap()
 			{
